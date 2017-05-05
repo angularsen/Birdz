1334015f40793fd834fbb616143ed2c79ef65b3c
Andreas Gullberg Larsen <anjdreas@gmail.com>
2017-05-06 00:45:52 +0200

    Add flapping audio effect

---
+++ Assets/AnimationSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSoundPlayer : MonoBehaviour {
+
+    private AudioSource _audioFlap;
+
+	// Use this for initialization
+	void Start () {
+        _audioFlap = GameObject.Find("AudioSourceFlap").GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnAnimationFlapStart()
+    {
+        _audioFlap.Play();
+    }
+}
